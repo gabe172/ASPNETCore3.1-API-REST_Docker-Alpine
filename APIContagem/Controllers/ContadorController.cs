16087a09285522c8cfbfa62c1334858272ad929d
GabrielGui <gabriel.guilherme88@gmail.com>
2023-08-31 10:27:51 -0300

    Update ContadorController.cs

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Azure Web App for Containers - Deployment Automatizado. . . - Okay",
+                    Local = "Azure Web App for Containers - Deployment Automatizado. . . -  GRupo RBS",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
